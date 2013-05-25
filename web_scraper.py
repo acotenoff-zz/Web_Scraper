79fd431aeb9d36998c8ee5c99f22c069d18f8c0f
Adam Cotenoff <acotenoff@gmail.com>
2013-05-25 19:44:26 -0400

    scrapes webpages for email addresses

--- web_scraper.py
+++ web_scraper.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
-"""web_scraper.py: Requests a web page and scrapes for email addresses """
+"""web_scraper.py: Requests a web page and scrapes for email addresses"""
 
 import urllib2
 import re
 import sys
 
+"""requests the given url and returns the html code"""
 def requestPage(url):
-		""" error handling for the url """
+		"""error handling for the url"""
 		try:
 			response = urllib2.urlopen("http://" + url)
 			print "Requesting page....\n"
@@ -15,16 +16,21 @@ def requestPage(url):
 			response.close()
 			print "Successfully requested " + response.geturl() + "\n"
 			return html
-			
+
 		except urllib2.HTTPError, e:
 			print 'HTTPError = ' + str(e.code)
 
 		except urllib2.URLError, e:
-			print 'URlError = ' + str(e.reason)
+			print 'URLError = ' + str(e.reason)
+
+"""given html code, searches it for email addresses using a regular expression"""
+def emailSearch(html):
+	
+
 
 def main():
 	url = raw_input("Please enter a website: http://")
-	requestPage(url)
+	emailSearch(requestPage(url))
 
 if __name__ == "__main__":
     main()
